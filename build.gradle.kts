900c5818444909a75edf7769319dc1fc246fbcf9
Gwanho Kim <khkim6040@gmail.com>
2026-09-20 22:55:40 +0900

    chore: add Redis container and spring-data-redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories { mavenCentral() }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
